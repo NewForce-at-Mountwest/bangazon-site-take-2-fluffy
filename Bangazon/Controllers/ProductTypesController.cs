cb85efc8b7acde9e81309b82a427e491a33ba7ef
sydneywait <sydneywait@gmail.com>
2019-06-18 11:29:11 -0400

    created views for product types

---
+++ Bangazon/Controllers/ProductTypesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Data;
+using Bangazon.Models;
+
+namespace Bangazon.Controllers
+{
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductType.ToListAsync());
+        }
+
+        // GET: ProductTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductType
+                .FirstOrDefaultAsync(m => m.ProductTypeId == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+   
+
+       
+        
+        private bool ProductTypeExists(int id)
+        {
+            return _context.ProductType.Any(e => e.ProductTypeId == id);
+        }
+    }
+}
