4208d92523b9da746fb1dd2f94c23195c3a8bfb0
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-06-21 14:42:57 -0400

    added view model for count

---
+++ Bangazon/Models/ProductViewModels/ProductCountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductCountViewModel
+    {
+        public IEnumerable<Product> products { get; set; } 
+        public IEnumerable<OrderProduct> orderProducts { get; set; }
+
+        public IEnumerable<Int32> counts { get; set; }
+    }
+}
