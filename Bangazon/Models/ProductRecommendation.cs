b561f207920ea8cbf1242c73f45a2de54dc08a66
sydneywait <sydneywait@gmail.com>
2019-06-21 09:06:07 -0400

    made join table for product recommendation

---
+++ Bangazon/Models/ProductRecommendation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class ProductRecommendation
+    {
+        [Required]
+        public string UserId { get; set; }
+        public string RecommenderId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public ApplicationUser User { get; set; }
+        public ApplicationUser Recommender { get; set; }
+
+    }
+}
